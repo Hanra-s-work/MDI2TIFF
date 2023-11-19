5a02b333e654741413a3d9a9fe322e5d09af6cb9
Henry LETELLIER <henry.letellier@epitech.eu>
2023-11-19 20:34:53 +0100

    [ADD] function in charge of displaying the content of the class

--- tests/test_mdi_to_tiff.py
+++ tests/test_mdi_to_tiff.py
@@ -13,3 +13,15 @@ def print_debug(string: str = "") -> None:
     debug = False
     if debug is True:
         print(f"DEBUG: {string}", file=stderr)
+
+
+def test_this_is_not_a_test() -> None:
+    """ Test the this is not a test function """
+    print("Testing this is not a test")
+    assert 0 == 0
+
+
+def test_class_content() -> None:
+    """ Test the class content """
+    print("Testing class content")
+    print(f"Displaying the content of mdi2img:\n{dir(mdi2img)}")
